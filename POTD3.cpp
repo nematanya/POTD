4514bf96b708a0241827e08d1f4e234f1d8ad879
Tanya Nema <tanyanema61@gmail.com>
2022-01-03 13:18:32 +0530

    3 JAN 2022

---
+++ POTD3.cpp
@@ -0,0 +1,62 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    void sort012(int a[], int n)
+    {
+        int i=0,s=0,e=n-1;
+    while(i<=e)
+    {
+        if(a[i]==2)
+        {
+        swap(a[i],a[e]);
+        e--;
+        }
+        else if(a[i]==0)
+        {
+            swap(a[i],a[s]);
+            s++;
+            i++;
+        }
+        else 
+        i++;
+    }
+    }
+    
+};
+
+// { Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >>n;
+        int a[n];
+        for(int i=0;i<n;i++){
+            cin >> a[i];
+        }
+
+        Solution ob;
+        ob.sort012(a, n);
+
+        for(int i=0;i<n;i++){
+            cout << a[i]  << " ";
+        }
+
+        cout << endl;
+        
+        
+    }
+    return 0;
+}
+
+  // } Driver Code Ends
